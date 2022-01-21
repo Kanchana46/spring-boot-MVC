c795b9b6ef8274fc62d06412abf3b61dfb4b348f
Kanchana46 <kanchana.rdg@gmail.com>
2022-01-21 15:57:38 +0530

    rest changes

--- src/main/java/com/code/webapp2/controller/UserController.java
+++ src/main/java/com/code/webapp2/controller/UserController.java
@@ -5,14 +5,22 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.code.webapp2.User;
 import com.code.webapp2.dao.UserRepo;
 
-@Controller
+@RestController
 public class UserController {
 	
 	@Autowired
@@ -52,5 +60,37 @@ public class UserController {
 		List<User> user = userRepo.findByNameSorted();
 		mv.addObject("user", user);
 		return mv;
+	}
+	
+	// Rest API implementation
+	@GetMapping("/users")
+	public List<User> getUserDetails() {
+		return userRepo.findAll();
+	}
+	
+	@GetMapping("/user/{id}")
+	public Optional<User> getUserByUserId(@PathVariable("id") int id) {
+		return userRepo.findById(id);
+	}
+	
+	@PostMapping("/addUser")
+	public User addUserDetails(@RequestBody User user) {
+		userRepo.save(user);
+		return user;
+	}
+	
+	@PutMapping("/user")
+	public User updateUser(@RequestBody User user) {
+		userRepo.save(user);
+		return user;
+	}
+	
+	@DeleteMapping("/user/{id}")
+	public int deleteUser(@PathVariable("id") int id) {
+		@SuppressWarnings("deprecation")
+		User user = userRepo.getOne(id);
+		System.out.println("ABC");
+		userRepo.delete(user);
+		return 1;
 	}
 }
--- src/main/java/com/code/webapp2/dao/UserRepo.java
+++ src/main/java/com/code/webapp2/dao/UserRepo.java
@@ -2,12 +2,13 @@ package com.code.webapp2.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.code.webapp2.User;
 
-public interface UserRepo extends CrudRepository<User, Integer>{
+public interface UserRepo extends JpaRepository<User, Integer>{
 	List<User> findByAge(int age);
 	
 	@Query("from User order by name")
